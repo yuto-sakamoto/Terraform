3a79c1f4942b053a4d82954da0c6e516fc8886ad
yuto sakamoto <y.m.a.c.p.r.o.s.11.9@gmail.com>
2021-12-19 18:58:15 +0900

    # RDSの作成に伴いリソース追加

--- cloudtech/vpc/main.tf
+++ cloudtech/vpc/main.tf
@@ -133,3 +133,107 @@ resource "aws_ecs_cluster" "cloudtech-test-cluster" {
   }
 }
 
+
+#############
+## RDS ##
+#############
+resource "aws_db_instance" "mysql" {
+  allocated_storage = 10
+  engine            = "mysql"
+  engine_version    = "8.0"
+  instance_class    = "db.t2.micro"
+  name              = "rds_development"
+  username          = "gkcadmin"
+  password          = "gkcadminaa"
+  # parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot    = true
+  storage_type           = "gp2"
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.cloudtech_subnet.name
+}
+
+
+# セキュリティグループ(RDS)
+resource "aws_security_group" "rds_sg" {
+  name        = "cloudtech_rds_sg"
+  description = "SecurityGroup of RDS"
+  vpc_id      = aws_vpc.cloudtech_ecs.id
+
+  egress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["192.0.2.0/32"]
+  }
+
+  tags = {
+    Name = "cloudtech-rds-sg"
+  }
+}
+# EC2セキュリティグループ
+resource "aws_security_group" "ec2_sg" {
+  name        = "cloudtech_ec2_sg"
+  description = "SecurityGroup of EC2"
+  vpc_id      = aws_vpc.cloudtech_ecs.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 53
+    to_port     = 53
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "cloudtech-ec2-sg"
+  }
+}
+resource "aws_security_group_rule" "rds-sg-ingress" {
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.rds_sg.id
+  source_security_group_id = aws_security_group.ec2_sg.id
+}
+
+resource "aws_security_group_rule" "rds-sg-egress" {
+  type              = "egress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ec2_sg.id
+}
+
+
+# サブネットグループ(rds-subnet)
+resource "aws_db_subnet_group" "cloudtech_subnet" {
+  name = "cloudtech-rds-sub"
+  subnet_ids = [
+    aws_subnet.private["private-1a"].id,
+    aws_subnet.private["private-1c"].id
+  ]
+
+  tags = {
+    Name = "cloudtech-rds-subnet-group"
+  }
+}
